69dd9edad8b64c0bff40bb0a83b4185e48da6dcc
Sarah Llewelyn <se.llewelyn@gmail.com>
2017-05-24 14:55:26 -0400

    rigid body 2d axis

---
+++ Assets/PlayerControl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+
+	private Rigidbody2D rb2d;
+
+	// Use this for initialization
+	void Start () {
+		rb2d = GetComponent<Rigidbody2D> ();
+	}
+	
+//	// Update is called once per frame before rendering a frame
+//	void Update () {
+//		
+//	}
+
+	// Called just before performing any physics calculations
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
+		rb2d.AddForce (movement);
+	}
+}
